c8f11d611a0fefaaed00c92eff5333ba777b7187
tylerkauff <tyler.kauffman@cognizant.com>
2022-03-09 11:42:09 -0600

    Styling and color

--- app/src/main/java/com/example/artsharekotlin/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/artsharekotlin/ui/dashboard/DashboardFragment.kt
@@ -13,32 +13,31 @@ import com.example.artsharekotlin.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
-    private lateinit var dashboardViewModel: DashboardViewModel
-    private var _binding: FragmentDashboardBinding? = null
+  private lateinit var dashboardViewModel: DashboardViewModel
+private var _binding: FragmentDashboardBinding? = null
+  // This property is only valid between onCreateView and
+  // onDestroyView.
+  private val binding get() = _binding!!
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        dashboardViewModel =
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+    _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+    val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
-    }
+    val textView: TextView = binding.textDashboard
+    dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
+      textView.text = it
+    })
+    return root
+  }
 
-    override fun onDestroyView() {
+override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
--- app/src/main/java/com/example/artsharekotlin/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/artsharekotlin/ui/notifications/NotificationsFragment.kt
@@ -13,32 +13,31 @@ import com.example.artsharekotlin.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
-    private var _binding: FragmentNotificationsBinding? = null
+  private lateinit var notificationsViewModel: NotificationsViewModel
+private var _binding: FragmentNotificationsBinding? = null
+  // This property is only valid between onCreateView and
+  // onDestroyView.
+  private val binding get() = _binding!!
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        notificationsViewModel =
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    notificationsViewModel =
             ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+    _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+    val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
-    }
+    val textView: TextView = binding.textNotifications
+    notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+      textView.text = it
+    })
+    return root
+  }
 
-    override fun onDestroyView() {
+override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
